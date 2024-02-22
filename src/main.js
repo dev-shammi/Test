3fdd27622bb79b3b7a7272b77a4599fd2811b983
dev-shammi <151466208+dev-shammi@users.noreply.github.com>
2024-02-22 15:53:10 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,40 +1,22 @@
-import { Client } from 'node-appwrite';
-
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
-
   // You can log messages to the console
   log('Hello, Logs!');
 
   // If something goes wrong, log an error
   error('Hello, Errors!');
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Set CORS headers for GET requests
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
+  // Parse the request body to extract additional data
+  const requestBody = JSON.parse(req.body);
 
-  // Set CORS headers for other requests
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // Access additional data sent by the client
+  const customHeaderData = requestBody.customHeaderData;
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+  // Use the additional data in your function logic
+  // For example, log or process the data
+  log('Custom Header Data:', customHeaderData);
+
+  // Send a response back to the client
+  return res.send('Function executed successfully!');
 };
