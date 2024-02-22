264f9461bc0fc078fd7cc1452af7e127cb3a2609
dev-shammi <151466208+dev-shammi@users.noreply.github.com>
2024-02-22 15:47:31 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,39 +1,40 @@
 import { Client } from 'node-appwrite';
-import cors from 'cors'; // Import the cors middleware
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Initialize the Appwrite client if needed
+  // Why not try the Appwrite SDK?
+  //
   // const client = new Client()
   //    .setEndpoint('https://cloud.appwrite.io/v1')
   //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
   //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // Enable CORS for all origins
-  const corsMiddleware = cors();
+  // You can log messages to the console
+  log('Hello, Logs!');
 
-  // Apply the CORS middleware to the request
-  corsMiddleware(req, res, () => {
-    // You can log messages to the console
-    log('Hello, Logs!');
+  // If something goes wrong, log an error
+  error('Hello, Errors!');
 
-    // If something goes wrong, log an error
-    error('Hello, Errors!');
+  // The `req` object contains the request data
+  if (req.method === 'GET') {
+    // Set CORS headers for GET requests
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Send a response with the res object helpers
+    // `res.send()` dispatches a string back to the client
+    return res.send('Hello, World!');
+  }
 
-    // The `req` object contains the request data
-    if (req.method === 'GET') {
-      // Send a response with the res object helpers
-      // `res.send()` dispatches a string back to the client
-      return res.send('Hello, World!');
-    }
+  // Set CORS headers for other requests
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    // `res.json()` is a handy helper for sending JSON
-    return res.json({
-      motto: 'Build like a team of hundreds_',
-      learn: 'https://appwrite.io/docs',
-      connect: 'https://appwrite.io/discord',
-      getInspired: 'https://builtwith.appwrite.io',
-    });
+  // `res.json()` is a handy helper for sending JSON
+  return res.json({
+    motto: 'Build like a team of hundreds_',
+    learn: 'https://appwrite.io/docs',
+    connect: 'https://appwrite.io/discord',
+    getInspired: 'https://builtwith.appwrite.io',
   });
 };
